a4c74da0dc25f2c9f360f2066fb6bc944c230e48
Bronek Szulc <bronek@fastmail.fm>
2024-11-02 13:07:19 -0400

    Rename run to start

--- api/main.go
+++ api/main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func Run() {
+func Start() {
 	e := echo.New()
 
 	e.Use(Logger())
--- cmd/piccolo/main.go
+++ cmd/piccolo/main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	api.Run()
+	api.Start()
 }
