55082e69247e9a7641a4aa6d04294d2557cec9a9
Bronek Szulc <bronek@fastmail.fm>
2024-11-03 11:06:03 -0500

    cleanup migrations

--- Makefile
+++ Makefile
@@ -7,4 +7,7 @@ run:
 	go run cmd/piccolo/main.go
 
 migrate_up:
-	migrate -verbose -database $(DB_LOCALHOST_URL)?sslmode=disable -path db/migrations up
+	migrate -database $(DB_LOCALHOST_URL)?sslmode=disable -path db/migrations up
+
+migrate_down:
+	migrate -database $(DB_LOCALHOST_URL)?sslmode=disable -path db/migrations down 1
--- db/migrations/000001_create_photos_table.up.sql
+++ db/migrations/000001_create_photos_table.up.sql
@@ -1,11 +1,3 @@
-CREATE OR REPLACE FUNCTION update_updated_at()
-RETURNS TRIGGER AS $$
-BEGIN
-  NEW.updated_at = NOW();
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
 create table if not exists photos(
   id uuid primary key default gen_random_uuid(),
   location text not null,
@@ -13,7 +5,7 @@ create table if not exists photos(
   updated_at timestamptz not null default now()
 );
 
-CREATE TRIGGER photos_update_updated_at
-BEFORE UPDATE ON photos
-FOR EACH ROW
-EXECUTE FUNCTION update_updated_at();
+create trigger photos_update_updated_at
+before update on photos
+for each row
+execute function update_updated_at();
