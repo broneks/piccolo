45959a51bdcc849115601115f4242613975eb271
teesloane <tylersloane@gmail.com>
2024-11-04 13:28:40 -0500

    Move redis code to redis package.

--- api/model/photo.go
+++ api/model/photo.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"fmt"
 	"piccolo/api/storage/wasabi"
+	"piccolo/api/storage/redis"
 	"time"
 
-	"github.com/go-redis/redis/v8"
 	"github.com/labstack/echo/v4"
 )
 
@@ -21,11 +21,11 @@ type Photo struct {
 }
 
 func (p *Photo) GetUrl(c echo.Context) string {
-	val, _ := getFromRedis(c, p.Filename)
+	val, _ := redis.GetFromRedis(c, p.Filename)
 	if val == "" {
 		fmt.Println("no cache found for url image.")
 		url, _ := wasabi.GetPresignedUrl(context.Background(), p.Filename)
-		setInRedis(c, p.Filename, url)
+		redis.SetInRedis(c, p.Filename, url)
 		return url
 	} else {
 		fmt.Println("found cached url for image")
@@ -33,26 +33,26 @@ func (p *Photo) GetUrl(c echo.Context) string {
 	}
 }
 
-func getFromRedis(c echo.Context, key string) (string, error) {
-	reqCtx := c.Request().Context()
-	rdb := c.Get("redisClient").(*redis.Client)
+// func getFromRedis(c echo.Context, key string) (string, error) {
+// 	reqCtx := c.Request().Context()
+// 	rdb := c.Get("redisClient").(*redis.Client)
 
-	val, err := rdb.Get(reqCtx, key).Result()
-	if err != nil {
-		return "", fmt.Errorf("failed to get value from Redis: %w", err)
-	}
+// 	val, err := rdb.Get(reqCtx, key).Result()
+// 	if err != nil {
+// 		return "", fmt.Errorf("failed to get value from Redis: %w", err)
+// 	}
 
-	return val, nil
-}
+// 	return val, nil
+// }
 
-func setInRedis(c echo.Context, key string, value string) error {
-	reqCtx := c.Request().Context()
-	rdb := c.Get("redisClient").(*redis.Client)
+// func setInRedis(c echo.Context, key string, value string) error {
+// 	reqCtx := c.Request().Context()
+// 	rdb := c.Get("redisClient").(*redis.Client)
 
-	err := rdb.Set(reqCtx, key, value, 0).Err() // 0 means no expiration
-	if err != nil {
-		return fmt.Errorf("failed to set value in Redis: %w", err)
-	}
+// 	err := rdb.Set(reqCtx, key, value, 0).Err() // 0 means no expiration
+// 	if err != nil {
+// 		return fmt.Errorf("failed to set value in Redis: %w", err)
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
