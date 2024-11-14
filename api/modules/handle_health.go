ae816b797dc850f5342d664ecf611433d45286bf
Bronek Szulc <bronek@fastmail.fm>
2024-11-14 12:16:28 -0500

    rename in error

--- api/modules/handle_health.go
+++ api/modules/handle_health.go
@@ -25,7 +25,7 @@ func handleHealth(server *types.Server) echo.HandlerFunc {
 		}
 
 		if err = server.ObjectStorage.Ping(ctx); err != nil {
-			server.Logger.Error(fmt.Sprintf("wasabi ping failed: %v", err.Error()))
+			server.Logger.Error(fmt.Sprintf("backblaze ping failed: %v", err.Error()))
 			return c.NoContent(http.StatusServiceUnavailable)
 		}
 
