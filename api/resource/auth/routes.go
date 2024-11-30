80581932ae496b0d4c209ba3c1016af4c9242c64
Bronek Szulc <bronek@fastmail.fm>
2024-11-30 15:06:41 -0500

    temporarily block auth routes

--- api/resource/auth/routes.go
+++ api/resource/auth/routes.go
@@ -1,14 +1,21 @@
 package auth
 
 import (
+	"os"
+
 	"github.com/labstack/echo/v4"
 )
 
 func (mod *AuthModule) Routes(g *echo.Group) {
 	auth := g.Group("/auth")
 
+	if os.Getenv("ENV") != "local" {
+		return
+	}
+
 	auth.POST("/register", mod.registerHandler)
 	auth.POST("/login", mod.loginHandler)
+
 	auth.POST("/refresh", mod.refreshHandler)
 	auth.POST("/logout", mod.logoutHandler)
 	auth.POST("/forgot-password", mod.forgotPasswordHandler)
