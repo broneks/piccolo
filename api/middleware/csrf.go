f6719a03e77470785c194e97bb0e47e0e0c7c335
Bronek Szulc <bronek@fastmail.fm>
2024-11-30 14:41:23 -0500

    csrf token as cookie

--- api/middleware/csrf.go
+++ api/middleware/csrf.go
@@ -1,27 +1,16 @@
 package middleware
 
 import (
-	"strings"
-
 	"github.com/labstack/echo/v4"
 	echoMiddleware "github.com/labstack/echo/v4/middleware"
 )
 
-func csrfSkipper(c echo.Context) bool {
-	if Skipper(c) {
-		return true
-	}
-
-	url := c.Request().URL.String()
-
-	return strings.HasPrefix(url, "/api")
-}
-
 func Csrf() echo.MiddlewareFunc {
 	return echoMiddleware.CSRFWithConfig(echoMiddleware.CSRFConfig{
-		Skipper:        csrfSkipper,
-		TokenLookup:    "form:csrf",
-		CookieName:     "_csrf",
+		Skipper:        Skipper,
+		TokenLookup:    "cookie:_csrf",
+		CookiePath:     "/",
+		CookieDomain:   "piccolo.pics",
 		CookieSecure:   true,
 		CookieHTTPOnly: true,
 	})
