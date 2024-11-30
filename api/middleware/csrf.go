77516e9055f8520059bd5c6b4203ffb4453962aa
Bronek Szulc <bronek@fastmail.fm>
2024-11-30 14:26:11 -0500

    update csrf skipper

--- api/middleware/csrf.go
+++ api/middleware/csrf.go
@@ -1,13 +1,25 @@
 package middleware
 
 import (
+	"strings"
+
 	"github.com/labstack/echo/v4"
 	echoMiddleware "github.com/labstack/echo/v4/middleware"
 )
 
+func csrfSkipper(c echo.Context) bool {
+	if Skipper(c) {
+		return true
+	}
+
+	url := c.Request().URL.String()
+
+	return strings.HasPrefix(url, "/api")
+}
+
 func Csrf() echo.MiddlewareFunc {
 	return echoMiddleware.CSRFWithConfig(echoMiddleware.CSRFConfig{
-		Skipper:        Skipper,
+		Skipper:        csrfSkipper,
 		TokenLookup:    "form:csrf",
 		CookieName:     "_csrf",
 		CookieSecure:   true,
